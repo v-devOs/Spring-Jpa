a10b72766edf7f345a076cbecb18cd3e9f54cb2f
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-08-31 14:20:50 -0600

    Usando interfaz CrudRepository

--- src/main/java/com/example/IntroSpringDataJpa/IntroSpringDataJpaApplication.java
+++ src/main/java/com/example/IntroSpringDataJpa/IntroSpringDataJpaApplication.java
@@ -1,6 +1,5 @@
 package com.example.IntroSpringDataJpa;
 
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 
 import com.example.IntroSpringDataJpa.persistence.entity.Customer;
 import com.example.IntroSpringDataJpa.persistence.repository.CustomerCrudRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 public class IntroSpringDataJpaApplication {
@@ -28,17 +29,33 @@ public class IntroSpringDataJpaApplication {
 			juan.setName("Juan Lopez");
 			juan.setPassword("juan123");
 
-			customerCrudRepository.save(juan);
+//			Customer ramon = new Customer();
+//			ramon.setName("ramon Lopez");
+//			ramon.setPassword("ramon123");
+//
+//			Customer luis = new Customer();
+//			luis.setName("luis marquez");
+//			luis.setPassword("luis123");
 
-			// System.out.println("Se guardo la entidad juan");
-
-			// System.out.println("\n Imprimiendo todos los clientes");
-			// customerCrudRepository.findAll()
-			// 	.forEach( each -> System.out.println(each));
+//			System.out.println("Se guardaron 3 entidades");
+//			List<Customer> clientes = List.of(juan, ramon, luis);
+//
+//			customerCrudRepository.saveAll(clientes);
+//
+//			System.out.println("Se guardo la entidad juan");
+//
+//			System.out.println("\n Imprimiendo todos los clientes");
+//			customerCrudRepository.findAll()
+//				.forEach(System.out::println);
 
 			System.out.println("\nBuscando al cliente juan");
-			customerCrudRepository.findById(1L)
-			.ifPresent(System.out::println);
+			customerCrudRepository.findById(2L).ifPresent( cliente -> {
+				cliente.setName("Uriel");
+
+				customerCrudRepository.save( cliente );
+			});
+
+
 
 
 			System.out.println("\nEliminando al cliente juan");
--- src/main/java/com/example/IntroSpringDataJpa/persistence/repository/CustomerCrudRepository.java
+++ src/main/java/com/example/IntroSpringDataJpa/persistence/repository/CustomerCrudRepository.java
@@ -1,19 +1,10 @@
 package com.example.IntroSpringDataJpa.persistence.repository;
 
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.example.IntroSpringDataJpa.persistence.entity.Customer;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface CustomerCrudRepository extends Repository<Customer, Long>{
+public interface CustomerCrudRepository extends CrudRepository<Customer, Long>{
   
-  List<Customer> findAll();
-
-  Optional<Customer> findById(Long id);
-
-  Customer save(Customer customer);
-
-  void deleteById(Long id);
+ 
 }
